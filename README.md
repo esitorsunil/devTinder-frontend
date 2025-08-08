55031507ab782dca98d931f13f24d77f817b91d4
esitorsunil <sunilrio23@gmail.com>
2025-08-08 16:36:59 +0530

    redirect

--- README.md
+++ README.md
@@ -1,21 +1,29 @@
 ## DevTinder Frontend
  
+ ### Project Structure
  - Create a vite+react app structure
  - Remove unneccessary code, start with hello world
  - Install tailwind and daisyui
  - Add Navbar component to app.jsx
  
+ ### Router 
  - Install react-router-dom
  - create browserrouter > Routes > route
  - Body Component - Navbar , Outlet , Footer
 
+ ### Connect backend to frontend (cors)
  - LoginPage UI
  - handleLogin using axios fetch from backend
  - CORS - install cors in backend to connect backend and frontend => add middelware with config: set origin, credintials: true
  - whenever making an api call with axios add {withcredintials:true} - useful to add cookies token in browser
 
+ ### Redux Setup
  - Install @reduxjs/toolkit, react-redux
  - Configure appStore => Provider => CreateSlice => add Reducer => Dispatch an action => useSelector subscribe to store
  - Login and see if data coming prperly in store (dispatch)
  - Navbar should update if user login (useSelector)
  - Structure the component folder structure
+
+ ### Redirect to login not authenticated
+ - should not access across routes
+ - if token is not present , redirect to login page
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom'
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const NavBar = () => {
+  const user = useSelector((store) => store.user);
+  return (
+  
+    <div className="navbar bg-base-300 shadow-sm">
+    <div className="flex-1">
+      <Link to = "/" className="btn btn-ghost text-xl">DevTinder</Link>
+    </div>
+    <div className="flex gap-2">
+    {user && (
+        <div className='flex items-center'>
+          <p>Welcome, {user.firstName}</p>
+      <div className="dropdown dropdown-end mx-5">
+        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+          <div className="w-10 rounded-full">
+            <img
+              alt="Tailwind CSS Navbar component"
+              src= {user.photoUrl} />
+          </div>
+        </div>
+        <ul
+          tabIndex={0}
+          className="menu menu-sm dropdown-content bg-base-300 rounded-box z-1 mt-3 w-52 p-2 shadow ">
+          <li>
+            <Link to = "/profile" className="justify-between">
+              Profile
+              <span className="badge">New</span>
+            </Link>
+          </li>
+          <li><a>Settings</a></li>
+          <li><a>Logout</a></li>
+        </ul>
+      </div>
+      </div>
+    )}
+    </div>
+  </div>
+
+  )
+}
+
+export default NavBar
