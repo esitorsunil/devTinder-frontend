7310ccee3d95dc41bc595484aa73359600e33b64
esitorsunil <sunilrio23@gmail.com>
2026-01-27 21:51:40 +0530

    chat: send , receive msg via room

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,10 +1,46 @@
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom'
+import { createSocketConnection } from '../utils/socket';
 
 const Chat = () => {
     const { targetUserId } = useParams();
     const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessage] = useState("");
+
+    const user = useSelector((store) => store.user);
+    const userId = user?._id;
+
+    useEffect(() => {
+
+      if(!userId) {
+        return;
+      }
+        const socket = createSocketConnection();
+
+        socket.emit("joinChat", {firstName: user.firstName, userId, targetUserId});
+
+        socket.on("messageReceived", ({firstName, text}) => {
+          console.log(firstName+ " "+ text);
+          setMessages((messages) => [...messages, {firstName, text}]);
+        })
+
+        return () => {
+            socket.disconnect();
+        }
+    }, [userId, targetUserId]);
+
+    const sendMessage = () => {
+      const socket = createSocketConnection();
+      socket.emit("sendMessage", {
+        firstName: user.firstName,
+        userId,
+        targetUserId,
+        text: newMessage,
+      });
+      setNewMessage("");
+    }
 
   return (
     <div className="w-3/4 mx-auto border border-gray-600 m-5 h-[70vh] flex flex-col">
@@ -20,7 +56,7 @@ const Chat = () => {
               }
             >
               <div className="chat-header">
-              
+                {msg.firstName}
                 <time className="text-xs opacity-50"> 2 hours ago</time>
               </div>
               <div className="chat-bubble">{msg.text}</div>
@@ -31,10 +67,13 @@ const Chat = () => {
       </div>
       <div className="p-5 border-t border-gray-600 flex items-center gap-2">
         <input
-          
+          value={newMessage}
+          onChange={(e) => setNewMessage(e.target.value) }
           className="flex-1 border border-gray-500 text-black rounded p-2"
         ></input>
-        <button className="btn btn-secondary">
+        <button 
+        onClick={sendMessage}
+        className="btn btn-secondary">
           Send
         </button>
       </div>
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,1 @@
-export const BASE_URL =  "https://devtinder23.onrender.com" ;
+export const BASE_URL = "http://localhost:3000" ;
